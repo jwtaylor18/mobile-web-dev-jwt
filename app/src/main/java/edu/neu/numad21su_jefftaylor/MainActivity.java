1adfdda2e6b3a0982c67c338ed57908575c4e249
Jeff Taylor <jeff.taylor@slalom.com>
2021-05-28 16:55:48 -0400

    launching new activity from a button

--- app/src/main/java/edu/neu/numad21su_jefftaylor/MainActivity.java
+++ app/src/main/java/edu/neu/numad21su_jefftaylor/MainActivity.java
@@ -2,6 +2,7 @@ package edu.neu.numad21su_jefftaylor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -17,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     public void showInfo(View view) {
         TextView textView = findViewById(R.id.info);
         textView.setText("Jeff Taylor taylor.je@northeastern.edu");
+    }
+
+    public void openClickyClicky(View view) {
+        Intent intent = new Intent(this, ClickyClicky.class);
+        startActivity(intent);
     }
 }
