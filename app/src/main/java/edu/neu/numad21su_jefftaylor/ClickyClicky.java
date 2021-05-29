55d85ce1616b36fb4a3714ad44773f8a7b622e1e
Jeff Taylor <jeff.taylor@slalom.com>
2021-05-29 16:02:15 -0400

    showing pressed button in top left corner

--- app/src/main/java/edu/neu/numad21su_jefftaylor/ClickyClicky.java
+++ app/src/main/java/edu/neu/numad21su_jefftaylor/ClickyClicky.java
@@ -3,6 +3,9 @@ package edu.neu.numad21su_jefftaylor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ClickyClicky extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class ClickyClicky extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_clicky);
+    }
+
+    public void showPressedButton(View view) {
+        Button b = (Button) view;
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText("Pressed: " + b.getText().toString());
     }
 }
