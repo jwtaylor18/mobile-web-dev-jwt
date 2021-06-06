7ad4206d463501d4e8cf31c472e9e47632a4df28
Jeff Taylor <jeff.taylor@slalom.com>
2021-06-05 21:50:02 -0400

    working add url

--- app/src/main/java/edu/neu/numad21su_jefftaylor/LinkCollector.java
+++ app/src/main/java/edu/neu/numad21su_jefftaylor/LinkCollector.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -19,6 +21,9 @@ public class LinkCollector extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter rviewAdapter;
     private RecyclerView.LayoutManager rLayoutManger;
+    private EditText et_UrlName;
+    private EditText et_Url;
+    private Button addURL;
     private FloatingActionButton addButton;
 
     private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
@@ -29,6 +34,17 @@ public class LinkCollector extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector2);
         init(savedInstanceState);
+
+        et_UrlName = findViewById(R.id.et_UrlName);
+        et_Url = findViewById(R.id.et_url);
+        addURL = findViewById(R.id.btn_add);
+        addURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = 0;
+                addItem(pos);
+            }
+        });
     }
 
 
@@ -36,55 +52,50 @@ public class LinkCollector extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
 
-
         int size = itemList == null ? 0 : itemList.size();
         outState.putInt(NUMBER_OF_ITEMS, size);
 
         for (int i = 0; i < size; i++) {
-
             outState.putString(KEY_OF_INSTANCE + i + "0", itemList.get(i).getUrlName());
-
             outState.putString(KEY_OF_INSTANCE + i + "1", itemList.get(i).getUrl());
-
         }
         super.onSaveInstanceState(outState);
     }
 
-
     private void init(Bundle savedInstanceState) {
 
-        initialItemData(savedInstanceState);
+//        initialItemData(savedInstanceState);
         createRecyclerView();
     }
 
 
-    private void initialItemData(Bundle savedInstanceState) {
-
-        // Not the first time to open this Activity
-        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
-            if (itemList == null || itemList.size() == 0) {
-
-                int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
-
-                // Retrieve keys we stored in the instance
-                for (int i = 0; i < size; i++) {
-                    String urlName = savedInstanceState.getString(KEY_OF_INSTANCE + i + "0");
-                    String url = savedInstanceState.getString(KEY_OF_INSTANCE + i + "1");
-
-                    ItemCard itemCard = new ItemCard(urlName, url);
-
-                    itemList.add(itemCard);
-                }
-            }
-        }
-        // The first time to opne this Activity
-        else {
-            itemList.add(new ItemCard("Google", "https://www.google.com/"));
-            itemList.add(new ItemCard("ESPN", "https://www.espn.com/"));
-            itemList.add(new ItemCard("Northeastern", "https://www.northeastern.edu/"));
-        }
-
-    }
+//    private void initialItemData(Bundle savedInstanceState) {
+//
+//        // Not the first time to open this Activity
+//        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+//            if (itemList == null || itemList.size() == 0) {
+//
+//                int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
+//
+//                // Retrieve keys we stored in the instance
+//                for (int i = 0; i < size; i++) {
+//                    String urlName = savedInstanceState.getString(KEY_OF_INSTANCE + i + "0");
+//                    String url = savedInstanceState.getString(KEY_OF_INSTANCE + i + "1");
+//
+//                    ItemCard itemCard = new ItemCard(urlName, url);
+//
+//                    itemList.add(itemCard);
+//                }
+//            }
+//        }
+//        // The first time to opne this Activity
+//        else {
+//            itemList.add(new ItemCard("Google", "https://www.google.com/"));
+//            itemList.add(new ItemCard("ESPN", "https://www.espn.com/"));
+//            itemList.add(new ItemCard("Northeastern", "https://www.northeastern.edu/"));
+//        }
+//
+//    }
 
     private void createRecyclerView() {
 
@@ -99,7 +110,7 @@ public class LinkCollector extends AppCompatActivity {
                 //attributions bond to the item has been changed
                 itemList.get(position).onItemClick(position, v);
 
-                rviewAdapter.notifyItemChanged(position);
+//                rviewAdapter.notifyItemChanged(position);
             }
 
 //            @Override
@@ -114,5 +125,12 @@ public class LinkCollector extends AppCompatActivity {
 
         recyclerView.setAdapter(rviewAdapter);
         recyclerView.setLayoutManager(rLayoutManger);
+    }
+
+    private void addItem(int position) {
+        itemList.add(position, new ItemCard(et_UrlName.getText().toString(), et_Url.getText().toString()));
+//        Toast.makeText(MainActivity.this, "Add an item", Toast.LENGTH_SHORT).show();
+
+        rviewAdapter.notifyItemInserted(position);
     }
 }
