d6b73f99f3bebbe40cce447fc6f6a8c2f6f0f520
Jeff Taylor <jeff.taylor@slalom.com>
2021-06-13 12:02:31 -0400

    location implemented

--- app/src/main/java/edu/neu/numad21su_jefftaylor/Locator.java
+++ app/src/main/java/edu/neu/numad21su_jefftaylor/Locator.java
@@ -1,14 +1,73 @@
 package edu.neu.numad21su_jefftaylor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Locator extends AppCompatActivity {
+public class Locator extends AppCompatActivity implements LocationListener {
+
+    TextView latitudeDisplay;
+    TextView longitudeDisplay;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        latitudeDisplay = findViewById(R.id.latitude);
+        longitudeDisplay = findViewById(R.id.longitude);
+
+        if (ContextCompat.checkSelfPermission(Locator.this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Locator.this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION }, 100);
+
+        }
+
+        getLocation();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 5, Locator.this);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitudeDisplay.setText(String.valueOf(location.getLatitude()));
+        longitudeDisplay.setText(String.valueOf(location.getLongitude()));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 }
